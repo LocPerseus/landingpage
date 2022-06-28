8ba8e8e171338b0cbff9e125e627fab2e92e2312
LocPerseus <kinghoangloc@gmail.com>
2022-06-28 11:56:11 +0700

    Page 2

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,8 @@
-import Banner from "../sections/Banner";
-import Artists from "../sections/Artists";
+import Artists from "../sections/artist-list";
 import React from "react";
-import Service from "../sections/service";
-import Audio from "../sections/Audio";
-import Profit from "../sections/profit";
-import Benifit from "../sections/benifit.jsx";
-import About from "../sections/about";
 
 export default function Home() {
-  return (
-    <React.Fragment>
-      <Banner />
-      <Artists />
-      <Service />
-      <Audio />
-      <Profit />
-      <Benifit />
-      <About />
-    </React.Fragment>
-  );
+  return <React.Fragment>
+    <Artists/>
+  </React.Fragment>
 }
